f80d6c0d3aeade9ba34f075b2154f975774e3a4b
AJAmelse <aj.amelse@hopkinsrobotics.org>
2023-02-09 20:32:23 -0600

    Added REV library and created NEOtest subsystem

---
+++ src/main/java/frc/robot/subsystems/NEOTestSubsystem.java
@@ -0,0 +1,13 @@
+package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+
+public class NEOTestSubsystem extends SubsystemBase {
+    private final CANSparkMax testNEO = new CANSparkMax(0, MotorType.kBrushless);
+
+    
+}
