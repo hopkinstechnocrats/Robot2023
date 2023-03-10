bc6ecd5338afb248dba953fe9b7695ac9a64ca6c
Grig84 <44533697+Grig84@users.noreply.github.com>
2023-03-09 20:33:24 -0600

    Add PID values to Code

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -189,8 +189,8 @@ public final class Constants {
 
       public static final double kMaxExtentionFlat = -13.5;
     
-      public static final double kP = 1;
-      public static final double kI = 0;
+      public static final double kP = 0.035;
+      public static final double kI = 0.00001;
       public static final double kD = 0;
       public static final double kFF = 0;
       public static final double kIz = 0;
@@ -205,7 +205,7 @@ public final class Constants {
 
       public static final double k45DegreesRots = -40;
 
-      public static final double kP = 1;
+      public static final double kP = 0.08;
       public static final double kI = 0;
       public static final double kD = 0;
       public static final double kFF = 0;
